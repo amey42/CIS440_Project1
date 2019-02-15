1a18f47d1c788c03aaa1bbbb817d62afd219d273
jbbuchan <jbbuchan@asu.edu>
2019-02-15 08:46:35 -0700

    got asmx file to work with our js code for logging in. Now goes to the redirect page on successful login after checking our db for email and password

--- accountmanager/AccountServices.asmx.cs
+++ accountmanager/AccountServices.asmx.cs
@@ -34,22 +34,22 @@ namespace accountmanager
 			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
 			//here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
 			//NOTICE: we added admin to what we pull, so that we can store it along with the id in the session
-			string sqlSelect = "SELECT id, admin FROM account WHERE userid=@idValue and pass=@passValue";
+			string sqlSelect = "SELECT cust_email, cust_password FROM user WHERE cust_email=@idValue and cust_password=@passValue";
 
 			//set up our connection object to be ready to use our connection string
 			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
 			//set up our command object to use our connection, and our query
 			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
 
-			//tell our command to replace the @parameters with real values
-			//we decode them because they came to us via the web so they were encoded
-			//for transmission (funky characters escaped, mostly)
-			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
-			sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
+            //tell our command to replace the @parameters with real values
+            //we decode them because they came to us via the web so they were encoded
+            //for transmission (funky characters escaped, mostly)
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
+            sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
 
-			//a data adapter acts like a bridge between our command object and 
-			//the data we are trying to get back and put in a table object
-			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            //a data adapter acts like a bridge between our command object and 
+            //the data we are trying to get back and put in a table object
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
 			//here's the table we want to fill with the results from our query
 			DataTable sqlDt = new DataTable();
 			//here we go filling it!
@@ -61,8 +61,8 @@ namespace accountmanager
 				//if we found an account, store the id and admin status in the session
 				//so we can check those values later on other method calls to see if they 
 				//are 1) logged in at all, and 2) and admin or not
-				Session["id"] = sqlDt.Rows[0]["id"];
-				Session["admin"] = sqlDt.Rows[0]["admin"];
+				Session["cust_email"] = sqlDt.Rows[0]["cust_email"];
+				//Session["admin"] = sqlDt.Rows[0]["admin"];
 				success = true;
 			}
 			//return the result!
--- accountmanager/LoginPage.js
+++ accountmanager/LoginPage.js
@@ -1,83 +1,128 @@
 
 
-var db_server 	= "scrumlords.cis440.com";		// mySQL server
-var db_port     = "3306"						// port
-var db_username = "scrumlords"; 				// login name
-var db_password = "!!Cis440"; 					// login password
-var db_table 	= "user"; 						// database to use
-var selectStmt  = "";
-var id = document.getElementById("myEmail");
-var password = document.getElementById("myPassword");
-var mysql = require('mysql');
-var express = require('express');
-var session = require('express-session');
+//var db_server 	= "scrumlords.cis440.com";		// mySQL server
+//var db_port     = "3306"						// port
+//var db_username = "scrumlords"; 				// login name
+//var db_password = "!!Cis440"; 					// login password
+//var db_table 	= "user"; 						// database to use
+//var selectStmt  = "";
+//var id = document.getElementById("myEmail");
+//var password = document.getElementById("myPassword");
+//var mysql = require('mysql');
+//var express = require('express');
+//var session = require('express-session');
 
-var tempUsername = 0;
-var tempPassword = 0;
-var inputValid = false;
+//var tempUsername = 0;
+//var tempPassword = 0;
+//var inputValid = false;
 
-var connection = mysql.createConnection({
-  host     : 'localhost:3306',
-  port	   : '3306',
-  user     : 'scrumlords',
-  password : '!!Cis440',
-  database : 'restauranteer'
-});
+//var connection = mysql.createConnection({
+//  host     : 'localhost:3306',
+//  port	   : '3306',
+//  user     : 'scrumlords',
+//  password : '!!Cis440',
+//  database : 'restauranteer'
+//});
 
-function initialize(){
-	connection.connect();
-}
+//function initialize(){
+//	connection.connect();
+//}
 
-function submit(form){
+//function submit(form){
 
-	tempUsername = document.getElementById('myEmail').value;
-	tempPassword = document.getElementById('myPassword').value;
+//	tempUsername = document.getElementById('myEmail').value;
+//	tempPassword = document.getElementById('myPassword').value;
 
-	selectStmt = "Select * from user where Cust_email = '" + tempUsername + "' and Cust_password = '" + tempPassword + "';";
+//	selectStmt = "Select * from user where Cust_email = '" + tempUsername + "' and Cust_password = '" + tempPassword + "';";
 
-	runQuery();
+//	runQuery();
 
 
-}
+//}
 
-function runQuery() {
+//function runQuery() {
 
-		MySql.Execute(
-			db_server,				// mySQL server
-			db_username, 				// login name
-			db_password, 			// login password
-			"restauranteer", 			// database to use
-									// SQL query string:
-			selectStmt,
-	        function (data) {
-	        	console.log(data);
-	        	if(data.Result.length==[0]) 
-	        		{inputValid=false;
-	        			alert("Please enter valid email and password");}
-	        			else
-	        				{inputValid=true;
-	        			alert("login successful!")
-	        			window.location.href = "RedirectPage.html";
-        		}
-	    	}
-	    );
+//		MySql.Execute(
+//			db_server,				// mySQL server
+//			db_username, 				// login name
+//			db_password, 			// login password
+//			"restauranteer", 			// database to use
+//									// SQL query string:
+//			selectStmt,
+//	        function (data) {
+//	        	console.log(data);
+//	        	if(data.Result.length==[0]) 
+//	        		{inputValid=false;
+//	        			alert("Please enter valid email and password");}
+//	        			else
+//	        				{inputValid=true;
+//	        			alert("login successful!")
+//	        			window.location.href = "RedirectPage.html";
+//        		}
+//	    	}
+//	    );
 
-   }
+//   }
 
    function createNewAccount()
    {
-   		var fname = document.getElementByName('fname');
-   		var lname = document.getElementByName('lname');
-   		var email = document.getElementByName('email');
-   		var password = document.getElementByName('password');
+        var fname = document.getElementByName('fname');
+        var lname = document.getElementByName('lname');
+        var email = document.getElementByName('email');
+        var password = document.getElementByName('password');
 
-   		// code to pass variables to web services to see if email already exists
-   		// if it does not exist, call method to create new entry in db and navigate to homepage
-   		console.log(fname);
-   		console.log(lname);
-   		console.log(email);
-   		console.log(password);
-   		debugger;
+        // code to pass variables to web services to see if email already exists
+        // if it does not exist, call method to create new entry in db and navigate to homepage
+        console.log(fname);
+        console.log(lname);
+        console.log(email);
+        console.log(password);
+        // window.location='./loginPage.html'
+    }
 
-   		// window.location='./loginPage.html'
-   }
+    function LogOn(uid, pass)
+    {
+        var webMethod = "AccountServices.asmx/LogOn";
+        var parameters = "{\"uid\":\"" + encodeURI(uid) + "\",\"pass\":\"" + encodeURI(pass) + "\"}";
+
+        $.ajax({
+            //post is more secure than get, and allows
+            //us to send big data if we want.  really just
+            //depends on the way the service you're talking to is set up, though
+            type: "POST",
+            //the url is set to the string we created above
+            url: webMethod,
+            //same with the data
+            data: parameters,
+            //these next two key/value pairs say we intend to talk in JSON format
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            //jQuery sends the data and asynchronously waits for a response.  when it
+            //gets a response, it calls the function mapped to the success key here
+            success: function (msg) {
+                //the server response is in the msg object passed in to the function here
+                //since our logon web method simply returns a true/false, that value is mapped
+                //to a generic property of the server response called d (I assume short for data
+                //but honestly I don't know...)
+                if (msg.d) {
+                    //server replied true, so show the accounts panel
+                    alert("success!");
+                    window.location = './RedirectPage.html';
+                }
+                else {
+                    //server replied false, so let the user know
+                    //the logon failed
+                    alert("logon failed");
+                }
+            },
+            error: function (e) {
+                //if something goes wrong in the mechanics of delivering the
+                //message to the server or the server processing that message,
+                //then this function mapped to the error key is executed rather
+                //than the one mapped to the success key.  This is just a garbage
+                //alert becaue I'm lazy
+                alert("boo...");
+            }
+        });
+
+    }
