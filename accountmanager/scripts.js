6504eaaf308ef3e707699576c1779bc6514fd238
jbbuchan <jbbuchan@asu.edu>
2019-02-21 10:48:44 -0700

    cleaned up js file

--- accountmanager/scripts.js
+++ accountmanager/scripts.js
@@ -1,67 +1,4 @@
-
-
-//var db_server 	= "scrumlords.cis440.com";		// mySQL server
-//var db_port     = "3306"						// port
-//var db_username = "scrumlords"; 				// login name
-//var db_password = "!!Cis440"; 					// login password
-//var db_table 	= "user"; 						// database to use
-//var selectStmt  = "";
-//var id = document.getElementById("myEmail");
-//var password = document.getElementById("myPassword");
-//var mysql = require('mysql');
-//var express = require('express');
-//var session = require('express-session');
-
-//var tempUsername = 0;
-//var tempPassword = 0;
-//var inputValid = false;
-
-//var connection = mysql.createConnection({
-//  host     : 'localhost:3306',
-//  port	   : '3306',
-//  user     : 'scrumlords',
-//  password : '!!Cis440',
-//  database : 'restauranteer'
-//});
-
-//function initialize(){
-//	connection.connect();
-//}
-
-//function submit(form){
-
-//	tempUsername = document.getElementById('myEmail').value;
-//	tempPassword = document.getElementById('myPassword').value;
-
-//	selectStmt = "Select * from user where Cust_email = '" + tempUsername + "' and Cust_password = '" + tempPassword + "';";
-
-//	runQuery();
-
-
-//}
-
-//function runQuery() {
-
-//		MySql.Execute(
-//			db_server,				// mySQL server
-//			db_username, 				// login name
-//			db_password, 			// login password
-//			"restauranteer", 			// database to use
-//									// SQL query string:
-//			selectStmt,
-//	        function (data) {
-//	        	console.log(data);
-//	        	if(data.Result.length==[0]) 
-//	        		{inputValid=false;
-//	        			alert("Please enter valid email and password");}
-//	        			else
-//	        				{inputValid=true;
-//	        			alert("login successful!")
-//	        			window.location.href = "RedirectPage.html";
-//        		}
-//	    	}
-//	    );
-//   }
+//JavaScript functions for Restauranteer
 
     function LogOn(uid, pass)
     {
@@ -91,7 +28,7 @@
                 if (msg.d)
                 {
                     //server replied true, so show the accounts panel
-                    alert("success!");
+                    //alert("success!");
                     window.location = './homepage-try.html';
                 }
                 else
@@ -111,10 +48,9 @@
                 alert("boo...");
             }
         });
-
     }
 
-//passes account info to the server, to create an account request
+
     function CreateAccount(id, pass, fname, lname)
     {
         var webMethod = "AccountServices.asmx/RequestAccount";
